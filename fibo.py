d1d25b5beb483d8120437f5508780ed5b1b54af2
rvpmin <roxanaperezmedina6@gmail.com>
2023-10-11 12:48:19 -0600

    fibonacci sequence

--- fibo.py
+++ fibo.py
@@ -6,14 +6,13 @@
 
 N = 10
 a = 1
-b = 1
-vector=[1,1]
+b = 1 
+print(a)
+print(b)
 
 for x in range(1,N+1):
     c = a + b
-    vector.append(c)
+    print(c)
     a = b
     b = c
-
-print(vector)
     
